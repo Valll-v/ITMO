8e8dee248001daaa1c639e7b36c4e305c0ed7649
Valll-v <stepanenko.valera2013@gmail.com>
2023-12-16 19:23:55 +0300

    unfinished 3 lab

--- CII/lab3/lab3/pl/pl.py
+++ CII/lab3/lab3/pl/pl.py
@@ -1,26 +1,51 @@
-from swiplserver import PrologMQI, PrologThread
+# coding: utf-8
+from swiplserver import PrologMQI
+from pl.services import (update_info, init_patterns,
+                         form_query, questions, examples)
 
 
 class PrologWorker:
 
-    def __init__(self, knowledge=None, path=None):
+    def __init__(self, path):
         mqi = PrologMQI()
         self.pl = mqi.create_thread()
-        self.load_knowledge(knowledge, path)
-        self.executors = {}
+        self.load_knowledge_by_path(path)
+        self.info = [[], [], []]
 
-    def load_knowledge(self, knowledge: str, path: str):
-        if knowledge:
-            # TODO: Реализовать чтение базы знаний напрямую, а не из файла
-            pass
-        if path:
-            self.pl.query(f'consult("{path}")')
+    def load_knowledge_by_path(self, path):
+        self.pl.query(f'consult("{path}")')
 
     def execute(self, query: str):
-        for answer in self.pl.query(query):
+        gen = self.pl.query(query)
+        if not gen:
+            print('Запрос не вернул никаких результатов. Перепроверьте info:\n'
+                  'Ваш герой: {0}\n'
+                  'Ваша команда: {1}\n'
+                  'Ваши артефакты: {2}'.format(
+                    ''.join(self.info[0]),
+                    ', '.join(self.info[1]),
+                    ', '.join(self.info[2])))
+            return None
+        for answer in gen:
             yield answer
 
     def run(self):
-        for i in self.execute('perfect_team(X,Y,Z)'):
-            print(i)
+        while True:
+            info_type = input('Выберите, что хотите ввести: '
+                              'информация (i) или запрос (q): ')
+            if info_type not in ('i', 'q'):
+                print('Неверный вариант - выберите i или q.')
+                continue
+            elif info_type == 'i':
+                string = input('Введите ваш запрос. Примеры:\n{}\n'
+                               .format('\n'.join(examples)))
+                self.info = update_info(string, self.info)
+            else:
+                string = input(
+                    'Введите ваш запрос. Примеры:\n{}\n'.format(
+                        '\n'.join(questions.keys())
+                    )
+                )
+                query = form_query(string, self.info)
+                for answer in self.execute(query):
 
--- CII/lab3/lab3/pl/services.py
+++ CII/lab3/lab3/pl/services.py
@@ -1,2 +1,64 @@
-def string_to_query(string: str):
-    pass
+# coding: utf-8
+import re
+
+
+def my_hero_choise(info):
+    if info[0]:
+        print('У вас уже выбран герой.')
+        return None
+    elif len(info[1]) >= 3:
+        print('Ваша команда полная. Вы не можете выбрать себе героя.')
+        return None
+    match len(info[1]):
+        case 0:
+            return 'perfect_team(X,Y,Z)'
+        case 1:
+            return f'perfect_team(X,Y,{info[1][0]})'
+        case 2:
+            return f'perfect_team(X,{info[1][1]},{info[1][0]})'
+
+
+def my_team_hero_choise(info):
+    print(info)
+
+
+def my_item_choice(info):
+    print(info)
+
+
+examples = [
+    r'Я играю на slark',
+    r'У меня в команде есть cristal_maiden',
+    r'Я купил bloodstone',
+]
+
+
+init_patterns = [
+    r'Я играю на (.+)',
+    r'У меня в команде есть (.+)',
+    r'Я купил (.+)',
+]
+
+
+questions = {
+    'Кого мне пикнуть?': my_hero_choise,
+    'Что мне купить?': my_item_choice,
+    'Кого пикнуть моей команде?': my_team_hero_choise,
+}
+
+
+def update_info(string: str, info: list[list]):
+    for i, pattern in enumerate(init_patterns):
+        match = re.match(pattern, string, re.IGNORECASE)
+        if match is not None:
+            info[i].append(match.groups()[0])
+            return info
+    print('Неизвестный тип информации. Попробуйте другой.')
+    return info
+
+
+def form_query(string, info):
+    if string in questions:
+        return questions[string](info)
+    print('Неверный выбор вопроса')
+    return None
